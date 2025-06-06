762eebbf94703e7eb23fc117bc3c1db602d411d7
Yongsu Kim <yongsu.kim@ezar.co.kr>
2025-06-06 21:18:24 +0900

    chore(json-viewer): remove json-viewer package and related files

--- packages/json-viewer/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ScalarJsonViewer } from './ScalarJsonViewer.vue'
-export { default as ScalarJsonViewerModal } from './ScalarJsonViewerModal.vue'
-export type { ScalarJsonViewerProps, ScalarJsonViewerModalProps } from './types'
-export * from './useJsonViewer' 
